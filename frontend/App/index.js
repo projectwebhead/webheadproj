9c7b86764c071262c26ec8f558183d9c2fc83449
christian <ronquechristian@gmail.com>
2023-05-12 17:47:23 +0800

    minor changes

--- frontend/App/index.js
+++ frontend/App/index.js
@@ -1,9 +1,14 @@
 'use strict';
 
 const htmlFun1 = document.querySelector('.html-fun-1');
+const parent = document.querySelector('.parent-content');
+const parentCourse = document.querySelector('.course');
 
 htmlFun1.addEventListener('click', (e) => {
   e.preventDefault();
-  location.href = './../../main-content-html/html-content.html';
   console.log('test');
+  parentCourse.classList.add('hidden');
+  parent.classList.remove('hidden');
+  document.getElementById('btn-section').classList.remove('hidden');
+  document.querySelector('footer').classList.remove('hidden');
 });
